fbe1d90bce7fcd372704d96cc96b341d530fa22f
T_Muia <81361221+Dan-245@users.noreply.github.com>
2023-10-10 19:04:45 +0300

    Update idValidator.py

--- idValidator.py
+++ idValidator.py
@@ -1,6 +1,6 @@
 def is_keyword(input_str):
     # List of reserved keywords
-    keywords = ["int", "string", "float", "char", "long", "short", "double", "if", "else", "for", "while", "do", "break", "void", "continue"]
+    keywords = ["int", "string", "float", "char", "long", "short", "double", "if", "else", "for", "while", "do", "break", "continue"]
 
     if input_str in keywords:
         return True
@@ -20,27 +20,26 @@ def is_valid_identifier(input_str):
 
 
 def main():
-    flag = 0
-    keyword = ["int", "string", "float", "char", "long", "if", "else", "for", "while", "do", "break", "default"]
-
+    not_valid = 0
+    
     # Input
     a = input("Enter the identifier: ")
 
     if is_keyword(a):
-        flag = 1
+        not_valid = 1
         print(f"{a} is not valid, since it's a keyword")
     else:
-        flag = 0
+        not_valid = 0
 
     if a[0].isalpha():  # Checks if the first char is an alphabetical letter
         for char in a[1:]:  # Checks if every other char is an alphanumeric symbol or an underscore
             if not (char.isalnum() or char == '_'):
-                flag = 1
+                not_valid = 1
                 break
     else:
-        flag = 1
+        not_valid = 1
 
-    if flag == 0:
+    if not_valid == 0:
         print(f"{a} is a valid identifier.")
     else:
         print(f"{a} is not a valid identifier.")
